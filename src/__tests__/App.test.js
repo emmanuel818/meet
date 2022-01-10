03cb398d66c07ef518ed95867d4d3d2f477dd312
emmanuel818 <emmanuelg8182001@gmail.com>
2022-01-10 10:32:58 -0600

    Update App.js to render new component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
 
 describe('<App /> component', () => {
   //Const renders component. Call shallow function with App as a parameter before all the test
@@ -16,5 +17,8 @@ describe('<App /> component', () => {
   });
   test('render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+  test('render number of events', () => {
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   });
 });
