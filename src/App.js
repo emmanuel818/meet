9b0026292f9a444501c1365c514f35f84b475235
emmanuel818 <emmanuelg8182001@gmail.com>
2022-01-14 12:52:21 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     currentLocation: 'all',
   }
 
-  updateEvents = (location, numberOfEvents) => {
+  updateEvents = async (location, numberOfEvents) => {
     getEvents().then((events) => {
       const locationEvents = (location === 'all') ?
         events :
