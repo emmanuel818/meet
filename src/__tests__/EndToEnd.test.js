2dbb984c231b21bb3094a813ba2e67e7598f92f6
emmanuel818 <emmanuelg8182001@gmail.com>
2022-01-19 11:14:33 -0600

    Update

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -6,11 +6,7 @@ describe('show/hide an event details', () => {
   let page;
   beforeAll(async () => {
     jest.setTimeout(30000);
-    browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 250,
-      ignoreDefaultArgs: ['--disable-extensions']
-    });
+    browser = await puppeteer.launch();
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
@@ -34,6 +30,6 @@ describe('show/hide an event details', () => {
   test('User can collapse an event to hide its details', async () => {
     await page.click('.event .hide-details-btn');
     const eventDetails = await page.$('.event .extra-details.hide');
-    expect(eventDetails).toBeNull;
+    expect(eventDetails).toBeNull();
   });
 })
