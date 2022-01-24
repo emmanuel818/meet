34e5c53ae9d9fe55323455b06d43655fac954686
emmanuel818 <emmanuelg8182001@gmail.com>
2022-01-24 14:18:54 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -54,7 +54,8 @@ class App extends Component {
   componentDidMount() {
     this.mounted = true;
     const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get('code');
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
